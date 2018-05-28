4c0c91c4bb59967db4e2b4e9b24a24b4c252c811
DreiParrent <dreiparrent@gmail.com>
2018-05-28 14:04:33 -0600

    Add project files.

---
+++ RaceAgainstSpace/RaceAgainstSpace/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RaceAgainstSpace.Models;
+
+namespace RaceAgainstSpace.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
